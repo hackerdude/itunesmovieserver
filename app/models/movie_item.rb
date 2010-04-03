ebf1f1afe9c32f3d51d7ab02b37cb432b41b9496
David Martinez <david@hackerdude.com>
2010-04-02 23:11:03 -0700

    Initial stab at putting some extra info on this thing.

--- app/models/movie_item.rb
+++ app/models/movie_item.rb
@@ -4,4 +4,16 @@ class MovieItem < ActiveRecord::Base
     MovieItem.delete_all
   end
 
+  # The human duration (the 
+  # total_time member is in 
+  # milliseconds)
+  def human_duration
+    hrs = ((total_time/1000)/60)/60
+    min = ((total_time/1000)/60) - (hrs*60)
+    result = ""
+    result += "#{hrs} hrs " if hrs > 0
+    result += "#{min} min" if min > 0
+    result.strip
+  end
+
 end
